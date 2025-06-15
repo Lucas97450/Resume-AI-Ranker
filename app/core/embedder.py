499ef731508ba4f610589aa8646cac9308a47b39
Lucas TIONOHOUE <lucastionohoue.pro@gmail.com>
2025-06-15 17:59:00 -0400

    Create embedder.py

---
+++ app/core/embedder.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import pickle
+
+def embed_all_cvs(folder, save_path="embeddings.pkl"):
+    # Load a pretrained Sentence Transformer model
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+    cvs_folder = Path(folder)
+    txt_files = cvs_folder.glob("*.txt")
+    embeddings_dict = {}
+    save_folder = Path(save_path)
+
+    for txt_file in txt_files:
+        text = txt_file.read_text(encoding="utf-8")
+        embeddings = model.encode(text)
+        print(embeddings.shape)
+        embeddings_dict[txt_file.name] = embeddings
+    print(embeddings_dict)
+
+
+    # Save the dictionary 
+    with open(save_path, "wb") as f:
+        pickle.dump(embeddings_dict, f)
+    
+    return embeddings_dict
+
+embed_all_cvs("parsed_cvs")
