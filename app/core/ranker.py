23252b951cb0f68a20a5e7396e4731c3c5aedb48
Lucas TIONOHOUE <lucastionohoue.pro@gmail.com>
2025-06-16 09:24:33 -0400

    Update ranker.py

--- app/core/ranker.py
+++ app/core/ranker.py
@@ -8,12 +8,29 @@ def similarity(cvs_path, jobs_path):
     cvs_files = cvs_folder.glob("*.pkl")
     jobs_folder = Path(jobs_path)
     jobs_files = jobs_folder.glob("*.pkl")
-        
+
+    cv_names = []
+    cv_vectors = []
+
     for cvs_file in cvs_files:
         for name, vec in utils.load_embeddings_items(cvs_file):
-            print(name ,  vec.shape)
+            cv_names.append(name)
+            cv_vectors.append(vec)
 
+    job_names = []
+    job_vectors = []
 
     for jobs_file in jobs_files:
         for name, vec in utils.load_embeddings_items(jobs_file):
-            print(name ,  vec.shape)
+            job_names.append(name)
+            job_vectors.append(vec)
+
+
+    vectors_similarity = []
+    
+    for job in job_vectors:
+        for cv in cv_vectors:
+            cos_sim = utils.cos_sim(job, cv)
+            vectors_similarity.append(cos_sim)
+        print(vectors_similarity)
+
