eaf63b281e00fc058749094cb035970cf77b20f2
Lucas TIONOHOUE <lucastionohoue.pro@gmail.com>
2025-06-16 07:43:33 -0400

    Update parser.py

--- app/core/parser.py
+++ app/core/parser.py
@@ -2,18 +2,18 @@ from pdfminer.high_level import extract_text
 from pathlib import Path
 
 # PDF parser function
-def parse_pdf(path):
-    path = Path(path)
-    pdf_text = extract_text(path)
+def parse_pdf(input_path, output_path):
+    path = Path(input_path)
+    pdf_text = extract_text(input_path)
 
     # Save in parsed_cvs
-    output_path = Path("parsed_cvs") / (path.stem + ".txt")
-    output_path.write_text(pdf_text, encoding="utf-8")
+    output_file = Path(output_path) / (path.stem + ".txt")
+    output_file.write_text(pdf_text, encoding="utf-8")
 
-def parse_all_pdf(folder):
-    pdf_folder = Path(folder)
+def parse_all_pdf(input_folder, output_folder):
+    pdf_folder = Path(input_folder)
     pdf_files = pdf_folder.glob("*.pdf")
     
     for pdf_path in pdf_files:
-        parse_pdf(pdf_path)
-        print(pdf_path)
+        parse_pdf(pdf_path, output_folder)
+        print(pdf_path, output_folder)
